ed3d7cecc8d7012d91bd9bf0e45eefb09d8339a1
Leetc0der <rishikkumar0308@gmail.com>
2024-07-26 23:01:03 +0530

    cors Implementation to allow wild card endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,21 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={
+    r"/*": {
+        "origins": "*",
+        "methods": ["GET", "POST", "PUT", "DELETE"],
+        "allow_headers": ["Content-Type", "Authorization"]
+        }
+    })
+
     app.config.from_object('config.Config')
     app.config['JWT_SECRET_KEY'] = app.config.get('SECRET_KEY')
     jwt = JWTManager(app)
